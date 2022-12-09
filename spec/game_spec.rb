ff41c601608905e3542165a8132db1acca3cc2c5
David Marino <davidjmarino8@gmail.com>
2022-12-09 16:49:44 -0500

    feat: update win/loss test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,22 +5,36 @@ require './lib/game'
 
 RSpec.describe Game do
     it "exists" do
-
         game = Game.new
-        require 'pry'; binding.pry
-        # @player_cruiser = Ship.new("Crusier", 3)
-        # @player_submerine = Ship.new("submerine", 2)
-        # @cpu_cruiser = Ship.new("Crusier", 3)
-        # @cpu_submerine = Ship.new("submerine", 2)
         
+        expect(game).to be_an_instance_of(Game)
+    end
+
+
+    it "determines a player loss" do
+        game = Game.new
+    
         expect(game).to be_an_instance_of(Game)
         game.player_cruiser.hit
         game.player_cruiser.hit
         game.player_cruiser.hit
         game.player_submarine.hit
-        player_submarine.hit
+        game.player_submarine.hit
         game.winner
-
-        
     end
+
+    it "determines a player win" do
+        game = Game.new
+    
+        expect(game).to be_an_instance_of(Game)
+        game.cpu_cruiser.hit
+        game.cpu_cruiser.hit
+        game.cpu_cruiser.hit
+        game.cpu_submarine.hit
+        game.cpu_submarine.hit
+        game.winner
+    end
+
+
+
 end
